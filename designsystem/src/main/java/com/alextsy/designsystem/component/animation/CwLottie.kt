0554852dc1006f6e9822c2e0bf01fc84a56dd59c
alextsybin <alexander.tsybin@gmail.com>
2025-05-01 20:45:45 +0200

    CW-15. Add Lottie animation

---
+++ designsystem/src/main/java/com/alextsy/designsystem/component/animation/CwLottie.kt
@@ -0,0 +1,32 @@
+package com.alextsy.designsystem.component.animation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+
+@Composable
+fun Lottie(
+    lottieUrl: String,
+    modifier: Modifier = Modifier
+) {
+    val preloaderLottieComposition by rememberLottieComposition(
+        LottieCompositionSpec.Url(url = lottieUrl)
+    )
+
+    val preloaderProgress by animateLottieCompositionAsState(
+        preloaderLottieComposition,
+        iterations = LottieConstants.IterateForever,
+        isPlaying = true
+    )
+
+    LottieAnimation(
+        modifier = modifier,
+        composition = preloaderLottieComposition,
+        progress = preloaderProgress
+    )
+}
