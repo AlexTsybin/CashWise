2b64e31c6404c5d057ab57c05d6b249549cb7a38
alextsybin <alexander.tsybin@gmail.com>
2025-05-01 16:53:34 +0200

    CW-10. Add designsystem module and theme for the app

---
+++ designsystem/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.alextsy.designsystem"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    api(libs.androidx.core.ktx)
+    api(libs.androidx.appcompat)
+    api(platform(libs.androidx.compose.bom))
+    api(libs.androidx.ui.tooling.preview)
+    api(libs.androidx.material3)
+    debugImplementation(libs.ui.tooling)
+    implementation(libs.lottie.compose)
+}
--- designsystem/src/main/java/com/alextsy/designsystem/theme/Type.kt
+++ designsystem/src/main/java/com/alextsy/designsystem/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.alextsy.cashwise.ui.theme
+package com.alextsy.designsystem.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
