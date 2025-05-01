7e97a094e0ef2e14bfe83976125aa2660218f2aa
alextsybin <alexander.tsybin@gmail.com>
2025-05-01 20:18:49 +0200

    CW-12. Create reusable component - button ans text

---
+++ designsystem/src/main/java/com/alextsy/designsystem/component/button/ButtonType.kt
@@ -0,0 +1,47 @@
+package com.alextsy.designsystem.component.button
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.alextsy.designsystem.component.text.TextType
+import com.alextsy.designsystem.theme.LocalDimensions
+
+enum class ButtonType {
+    FILLED_TONAL,
+    TEXT,
+    ELEVATED
+}
+
+fun ButtonType.getTextType() = when (this) {
+    ButtonType.FILLED_TONAL -> TextType.BUTTON_LABEL
+    ButtonType.TEXT -> TextType.LABEL_SMALL
+    ButtonType.ELEVATED -> TextType.LABEL_LARGE
+}
+
+@Composable
+fun ButtonType.getButtonElevation() = when (this) {
+    ButtonType.FILLED_TONAL -> ButtonDefaults.filledTonalButtonElevation()
+    ButtonType.TEXT -> ButtonDefaults.buttonElevation()
+    ButtonType.ELEVATED -> ButtonDefaults.elevatedButtonElevation()
+}
+
+@Composable
+fun ButtonType.getButtonColor() = when (this) {
+    ButtonType.FILLED_TONAL -> ButtonDefaults.buttonColors(
+        containerColor = MaterialTheme.colorScheme.primary,
+        contentColor = MaterialTheme.colorScheme.onPrimary
+    )
+    ButtonType.TEXT -> ButtonDefaults.textButtonColors()
+    ButtonType.ELEVATED -> ButtonDefaults.elevatedButtonColors()
+}
+
+@Composable
+fun Modifier.buttonWidth(isFullWith: Boolean) = if (isFullWith) {
+    fillMaxWidth()
+} else {
+    width(LocalDimensions.current.dimensions180)
+}.height(LocalDimensions.current.dimensions56)
---
+++ designsystem/src/main/java/com/alextsy/designsystem/component/preview/PreviewSurface.kt
@@ -0,0 +1,25 @@
+package com.alextsy.designsystem.component.preview
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.alextsy.designsystem.theme.CashWiseTheme
+
+@Composable
+fun PreviewSurface(content: @Composable () -> Unit) {
+    CashWiseTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                content()
+            }
+        }
+    }
+}
