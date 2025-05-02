dd78330fc70a2b6df1cfd04cf33231abb88a309e
Alexander <alexander.tsybin@gmail.com>
2025-05-02 16:06:47 +0300

    CW-13. Welcome screen UI (#10)

--- designsystem/src/main/java/com/alextsy/designsystem/theme/Theme.kt
+++ designsystem/src/main/java/com/alextsy/designsystem/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.alextsy.designsystem.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,7 +10,11 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = primary,
@@ -34,6 +39,8 @@ fun CashWiseTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
+    val view = LocalView.current
+
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
@@ -42,6 +49,14 @@ fun CashWiseTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.surface.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
     }
 
     CompositionLocalProvider(
---
+++ feature/onboarding/presentation/src/main/java/com/alextsy/onboarding/presentation/welcome/WelcomeScreen.kt
@@ -0,0 +1,101 @@
+package com.alextsy.onboarding.presentation.welcome
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.alextsy.designsystem.component.button.CwButton
+import com.alextsy.designsystem.component.preview.CwPreview
+import com.alextsy.designsystem.component.preview.PreviewSurface
+import com.alextsy.designsystem.component.text.CwText
+import com.alextsy.designsystem.component.text.TextType
+import com.alextsy.designsystem.theme.LocalDimensions
+import com.alextsy.onboarding.presentation.R
+
+@Composable
+fun WelcomeScreen(
+    onNextScreen: () -> Unit
+) {
+    Welcome(onNextScreen)
+}
+
+@Composable
+private fun Welcome(onNextScreen: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                start = LocalDimensions.current.dimensions16,
+                top = LocalDimensions.current.dimensions16,
+                end = LocalDimensions.current.dimensions16,
+                bottom = LocalDimensions.current.dimensions32
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = LocalDimensions.current.dimensions48),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Image(
+                painter = painterResource(id = com.alextsy.designsystem.R.drawable.welcome_logo),
+                modifier = Modifier
+                    .size(LocalDimensions.current.dimensions128)
+                    .padding(LocalDimensions.current.dimensions4)
+                    .clip(CircleShape),
+                contentDescription = "condition"
+            )
+            Column(
+                modifier = Modifier
+                    .padding(start = LocalDimensions.current.dimensions2)
+                    .align(Alignment.CenterVertically),
+                verticalArrangement = Arrangement.spacedBy(LocalDimensions.current.dimensions8),
+                horizontalAlignment = Alignment.End
+            ) {
+                CwText(
+                    text = stringResource(R.string.welcome_text),
+                    textType = TextType.TITLE
+                )
+                CwText(
+                    text = stringResource(R.string.welcome_app_name),
+                    textType = TextType.DISPLAY_SMALL_GRADIENT
+                )
+                CwText(
+                    text = stringResource(R.string.welcome_description),
+                    textType = TextType.LABEL_SMALL
+                )
+            }
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        CwButton(
+            text = stringResource(R.string.welcome_get_started),
+            isFullWidth = false,
+            onClick = {
+                onNextScreen()
+            }
+        )
+    }
+}
+
+@CwPreview
+@Composable
+private fun WelcomeScreenPreview() {
+    PreviewSurface {
+        WelcomeScreen(
+            onNextScreen = {}
+        )
+    }
+}
