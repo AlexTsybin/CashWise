dd187b1bd057cc49e0706c1f070fece2c78b3473
alextsybin <alexander.tsybin@gmail.com>
2025-05-04 15:29:59 +0200

    CW-27. Add Common model module

---
+++ common/model/src/main/java/com/alextsy/common/model/OnboardingConfig.kt
@@ -0,0 +1,7 @@
+package com.alextsy.common.model
+
+enum class OnboardingConfig {
+    NOT_STARTED,
+    IN_PROGRESS,
+    COMPLETED
+}
---
+++ common/model/src/main/java/com/alextsy/common/model/ThemeConfig.kt
@@ -0,0 +1,7 @@
+package com.alextsy.common.model
+
+enum class ThemeConfig {
+    SYSTEM,
+    LIGHT,
+    DARK
+}
