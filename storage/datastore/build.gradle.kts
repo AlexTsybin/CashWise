398276a2f3b84a29a97d0ef3310353c79706be91
Alexander <alexander.tsybin@gmail.com>
2025-05-05 15:54:08 +0300

    CW-28. Add Storage datastore module, proto files, serializer and data source (#16)

---
+++ storage/datastore/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.protobuf)
+}
+
+android {
+    namespace = "com.alextsy.storage.datastore"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+protobuf {
+    protoc {
+        artifact = libs.protobuf.protoc.get().toString()
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.builtins {
+                val java by registering { option("lite") }
+                val kotlin by registering { option("lite") }
+            }
+        }
+    }
+}
+
+dependencies {
+    api(projects.common.model)
+
+    api(libs.androidx.datastore.core)
+    api(libs.androidx.datastore)
+    api(libs.androidx.core.ktx)
+    api(platform(libs.koin.bom))
+    api(libs.koin.android)
+    api(libs.koin.core)
+    api(libs.protobuf.kotlin.lite)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ storage/datastore/src/main/java/com/alextsy/storage/datastore/user/UserPreferenceSerializer.kt
@@ -0,0 +1,29 @@
+package com.alextsy.storage.datastore.user
+
+import androidx.datastore.core.Serializer
+import com.alextsy.storage.datastore.data.UserPreferences
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.InputStream
+import java.io.OutputStream
+
+class UserPreferenceSerializer : Serializer<UserPreferences> {
+
+    override val defaultValue: UserPreferences = UserPreferences.getDefaultInstance()
+
+    override suspend fun readFrom(input: InputStream): UserPreferences =
+        withContext(Dispatchers.IO) {
+        runCatching {
+            UserPreferences.parseFrom(input)
+        }.getOrThrow()
+    }
+
+    override suspend fun writeTo(
+        t: UserPreferences,
+        output: OutputStream
+    ) {
+        withContext(Dispatchers.IO) {
+            t.writeTo(output)
+        }
+    }
+}
