ba422c247738e84f87e12757bb99d2426a22e05d
alextsybin <alexander.tsybin@gmail.com>
2025-05-08 23:26:09 +0200

    CW-38. Add DI to database module

---
+++ storage/database/src/main/java/com/alextsy/storage/database/di/Module.kt
@@ -0,0 +1,25 @@
+package com.alextsy.storage.database.di
+
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.alextsy.storage.database.FinanceDatabase
+import org.koin.dsl.module
+
+val roomModule = module {
+    single {
+        Room.databaseBuilder(
+            get(),
+            FinanceDatabase::class.java,
+            "finance_db"
+        ).addCallback(object : RoomDatabase.Callback() {
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+                FinanceDatabase.prepopulateDatabase(get())
+            }
+        })
+            .build()
+    }
+
+    single { get<FinanceDatabase>().financeDao() }
+}
