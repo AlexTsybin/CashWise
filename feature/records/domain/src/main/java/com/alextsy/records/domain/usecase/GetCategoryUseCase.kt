e2065adaa2b8032e20094397dfce0aa9ac6b2698
Alexander <alexander.tsybin@gmail.com>
2025-05-09 23:38:05 +0300

    CW-43. Update Records domain module (#25)
    * CW-43. Update Records domain module
    
    * CW-43. fix

---
+++ feature/records/domain/src/main/java/com/alextsy/records/domain/usecase/GetCategoryUseCase.kt
@@ -0,0 +1,18 @@
+package com.alextsy.records.domain.usecase
+
+import com.alextsy.common.model.records.RecordTransactionType
+import com.alextsy.records.domain.repository.UpdateFinanceRepository
+
+class GetCategoryUseCase(
+    private val repository: UpdateFinanceRepository,
+) {
+
+    suspend operator fun invoke(isIncome: Boolean) =
+        repository.getCategoriesWithSubcategories().filter {
+            if (isIncome) {
+                it.type == RecordTransactionType.CREDIT
+            } else {
+                it.type == RecordTransactionType.DEBIT
+            }
+        }
+}
